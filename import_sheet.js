07fd1254257c40cbca0231e692fffb9ad07bc896
John Aiken <johnm.aiken@gmail.com>
2018-11-03 20:18:47 +0100

    Delete import_sheet.js

--- import_sheet.js
+++
@@ -1,8 +0,0 @@
-function init() {
- Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/1KPuG5z52VWvloXocAwOXJf3iT56sTLi-TRSANyvMZws/pubhtml',
- callback: function(data, tabletop) { 
- console.log(data)
- },
- simpleSheet: true } )
-}
-window.addEventListener(‘DOMContentLoaded’, init)
